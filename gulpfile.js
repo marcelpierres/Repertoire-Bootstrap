ff19ddfca6651881e8ba8fba876b63db010772dc
marcelpierres <marcelpierre.samuels@ryerson.ca>
2018-09-09 02:54:37 -0400

    Iteration 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,122 @@
-var gulp = require("gulp");
-var browserSync = require("browser-sync").create();
-var sass = require("gulp-sass");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var pkg = require('./package.json');
+var browserSync = require('browser-sync').create();
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task("sass", function() {
-  return gulp
-    .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
-    .pipe(sass())
-    .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.stream());
-});
+// Set the banner content
+var banner = ['/*!\n',
+  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' */\n',
+  '\n'
+].join('');
 
-// Move the javascript files into our /src/js folder
-gulp.task("js", function() {
-  return gulp
-    .src([
-      "node_modules/bootstrap/dist/js/bootstrap.min.js",
-      "node_modules/jquery/dist/jquery.min.js",
-      "node_modules/popper.js/dist/umd/popper.min.js"
+// Copy third party libraries from /node_modules into /vendor
+gulp.task('vendor', function() {
+
+  // Bootstrap
+  gulp.src([
+      './node_modules/bootstrap/dist/**/*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
     ])
-    .pipe(gulp.dest("src/js"))
+    .pipe(gulp.dest('./vendor/bootstrap'))
+
+  // Font Awesome
+  gulp.src([
+      './node_modules/@fortawesome/**/*',
+    ])
+    .pipe(gulp.dest('./vendor'))
+
+  // jQuery
+  gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'))
+
+  // jQuery Easing
+  gulp.src([
+      './node_modules/jquery.easing/*.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery-easing'))
+
+});
+
+// Compile SCSS
+gulp.task('css:compile', function() {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass.sync({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('./css'))
+});
+
+// Minify CSS
+gulp.task('css:minify', ['css:compile'], function() {
+  return gulp.src([
+      './css/*.css',
+      '!./css/*.min.css'
+    ])
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
 });
 
-// Static Server + watching scss/html files
-gulp.task("serve", ["sass"], function() {
-  browserSync.init({
-    server: "./src"
-  });
+// CSS
+gulp.task('css', ['css:compile', 'css:minify']);
 
-  gulp.watch(
-    ["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"],
-    ["sass"]
-  );
-  gulp.watch("src/*.html").on("change", browserSync.reload);
+// Minify JavaScript
+gulp.task('js:minify', function() {
+  return gulp.src([
+      './js/*.js',
+      '!./js/*.min.js'
+    ])
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('./js'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task("default", ["js", "serve"]);
+// JS
+gulp.task('js', ['js:minify']);
+
+// Default task
+gulp.task('default', ['css', 'js', 'vendor']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+// Dev task
+gulp.task('dev', ['css', 'js', 'browserSync'], function() {
+  gulp.watch('./scss/*.scss', ['css']);
+  gulp.watch('./js/*.js', ['js']);
+  gulp.watch('./*.html', browserSync.reload);
+});
